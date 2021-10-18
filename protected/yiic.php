58d325c08ae8a6d7f3eb4568ad9d2b01f77c5ea1
Riajul-Islam-EWU <ritewu2014@gmail.com>
2021-10-18 13:56:26 +0600

    first commit

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic='C:\Users\HP\Downloads\Compressed\yii-1.1.24.a5ab20\framework\yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
